dfefd808676b3691e70b871c9d612e5d480669cd
LikeableJuniper <noa_steffen@hotmail.com>
2023-09-21 13:14:17 +0200

    Added error messages and load&save data

--- main.py
+++ main.py
@@ -16,21 +16,44 @@ GAME = 1
 class ButtonTypes:
     DEFAULT = 0
     CREATESAVE = 1
+    LOADSAVE = 2
+
+
+class AccountErrors:
+    PASSED = 0
+    EMPTY_CREDENTIALS = -1
+    USER_EXISTS = -2
+    NO_SAVE = -3
+    PASSWORD_INCORRECT = -4
+
 
 alphabet = [list("abcdefghijklmnopqrstuvwxyz"), list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")]
 numbers = list("0123456789")
 
+errorMessages = ["The password is incorrect.", "No save exists with that username.", "That username is already taken.", "Username or password cannot be empty."]
+
 
 def createSave(username: str, password: str):
+    if not (username and password):
+        return AccountErrors.EMPTY_CREDENTIALS
     if not os.path.isdir("Saves"):
         os.mkdir("Saves")
+    if f"{username}.json" in os.listdir("Saves"):
+        return AccountErrors.USER_EXISTS
     with open(f"Saves/{username}.json", "x") as f:
         saveData = {"username": username, "password": str(sha256(bytes(password.encode())).hexdigest())}
         json.dump(saveData, f)
+    return AccountErrors.PASSED
 
 
 def loadSave(username: str, password: str):
-    pass
+    if f"{username}.json" not in os.listdir("Saves"):
+        return AccountErrors.NO_SAVE
+    with open(f"Saves/{username}.json", "r") as f:
+        gameData = json.load(f)
+    if sha256(bytes(password.encode())).hexdigest() != gameData["password"]:
+        return AccountErrors.PASSWORD_INCORRECT
+    return gameData
 
 
 def locChange(newVal):
@@ -45,8 +68,25 @@ class Rectangle:
         self.color = color
 
 
+class Label:
+    def __init__(self, pos, text):
+        self.pos = pos
+        self.text = text
+        self.lastChange = 0 # How many frames since the value was last changed
+    
+    def render(self, screen: pg.Surface):
+        self.lastChange += 1
+        if self.lastChange >= 3000: self.text = ""
+        if self.text:
+            screen.blit(font.render(self.text, False, (255, 50, 50)), self.pos)
+    
+    def changeText(self, newText):
+        self.text = newText
+        self.lastChange = 0
+
+
 class Button(Rectangle):
-    def __init__(self, pos, dimensions, color, hoverColor, text="", onClick=None, buttonType=ButtonTypes.DEFAULT):
+    def __init__(self, pos, dimensions, color, hoverColor, text="", onClick=lambda: None, buttonType=ButtonTypes.DEFAULT):
         super().__init__(pos, dimensions, color)
         self.hoverColor = hoverColor
         self.text = text
@@ -55,10 +95,13 @@ class Button(Rectangle):
         self.buttonType = buttonType
     
     def __call__(self, **kwargs):
-        if self.buttonType == ButtonTypes.CREATESAVE:
-            createSave(kwargs["username"], kwargs["password"])
         if self.hovered:
+            if self.buttonType == ButtonTypes.CREATESAVE:
+                return createSave(kwargs["username"], kwargs["password"])
+            elif self.buttonType == ButtonTypes.LOADSAVE:
+                return loadSave(kwargs["username"], kwargs["password"])
             self.onClick()
+        return 0
     
     def render(self, screen, mousePos):
         if self.pos[0] < mousePos[0] < self.pos[0] + self.dimensions[0] and self.pos[1] < mousePos[1] < self.pos[1] + self.dimensions[1]:
@@ -121,7 +164,9 @@ def login():
     inputFields: list[Input] = [Input([500, 150], [150, 50], (200, 200, 100), "Name: "), Input([800, 150], [150, 50], (200, 200, 100), "Pswd: ")]
     lastFocused = 0 # Index of last focused on input field
 
-    buttons: list[Button] = [Button((10, 10), (100, 30), (240, 30, 30), (240, 60, 60), "Exit", onClick=lambda: locChange(-1))]
+    labels: list[Label] = [Label([500, 310], "")]
+
+    buttons: list[Button] = [Button((10, 10), (100, 30), (240, 30, 30), (240, 60, 60), "Exit", onClick=lambda: locChange(EXIT)), Button((500, 250), (150, 50), (30, 210, 170), (130, 255, 225), "Create Account", buttonType=ButtonTypes.CREATESAVE), Button([800, 250], [150, 50], (15, 75, 170), (80, 115, 170), "Load Save", buttonType=ButtonTypes.LOADSAVE)]
 
     while V_LOC == LOGIN:
         screen.fill((100, 100, 100))
@@ -131,7 +176,15 @@ def login():
         for button in buttons:
             button.render(screen, mousePos)
             if lclick:
-                button()
+                errCode = button(username=inputFields[0].value, password=inputFields[1].value)
+                if type(errCode) == dict:
+                    gameData = errCode
+                    continue
+                if errCode < 0:
+                    labels[0].changeText(errorMessages[errCode])
+        
+        for label in labels:
+            label.render(screen)
         
         pressed = pg.key.get_pressed()
         for i, inputField in enumerate(inputFields):
