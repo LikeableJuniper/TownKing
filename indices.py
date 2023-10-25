2d5a638af4d146913858b5b52aa10120324980fd
LikeableJuniper <noa_steffen@hotmail.com>
2023-10-25 14:28:34 +0200

    .

---
+++ indices.py
@@ -0,0 +1,22 @@
+class Locations:
+    EXIT = -1 # Anytime V_LOC is set to -1, exit the game
+    LOGIN = 0
+    GAME = 1
+
+class ButtonTypes:
+    DEFAULT = 0
+    CREATESAVE = 1
+    LOADSAVE = 2
+    EXIT = 3
+
+class AccountErrors:
+    PASSED = 0
+    EMPTY_CREDENTIALS = -1
+    USER_EXISTS = -2
+    NO_SAVE = -3
+    PASSWORD_INCORRECT = -4
+
+class Buildings:
+    EMPTY = 0
+    TOWNHALL = 1
+    CHURCH = 2
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from hashlib import sha256
 import os
 import json
 from vectors import Vector
+from indices import *
 
 pg.init()
 pg.font.init()
@@ -10,27 +11,6 @@ screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
 font = pg.font.Font(None, 25)
 
 V_LOC = 0
-class Locations:
-    EXIT = -1 # Anytime V_LOC is set to -1, exit the game
-    LOGIN = 0
-    GAME = 1
-
-class ButtonTypes:
-    DEFAULT = 0
-    CREATESAVE = 1
-    LOADSAVE = 2
-    EXIT = 3
-
-class AccountErrors:
-    PASSED = 0
-    EMPTY_CREDENTIALS = -1
-    USER_EXISTS = -2
-    NO_SAVE = -3
-    PASSWORD_INCORRECT = -4
-
-class Buildings:
-    EMPTY = 0
-    TOWNHALL = 1
 
 
 class Rectangle:
